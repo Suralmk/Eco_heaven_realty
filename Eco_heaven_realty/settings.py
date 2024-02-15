bbccc6600d44bc46480db868663fffdf4ec21112
Suralmk <104755355+Suralmk@users.noreply.github.com>
2024-02-15 11:49:38 +0300

    eco

--- Eco_heaven_realty/settings.py
+++ Eco_heaven_realty/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 ACCOUNT_USER_MODEL_USERNAME_FIELD = "email"
@@ -149,6 +149,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
+
+#STATICFILES_DIRS = os.path.join(BASE_DIR,'staticfiles/')
 
 MEDIA_URL = 'media/'
 
